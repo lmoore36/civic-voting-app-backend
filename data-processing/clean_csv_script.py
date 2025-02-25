2c654b22740b7f74b8fa9503c76ce0078e98c1bb
lmoore36 <lmoore36@unc.edu>
2025-02-25 13:38:19 +0000

    removing data from github

--- data-processing/clean_csv_script.py
+++ data-processing/clean_csv_script.py
@@ -7,9 +7,13 @@ columns_to_keep = [
     "voter_reg_num", 
     "last_name", 
     "first_name", 
-    "res_street_address", 
-    "res_city_desc", 
-    "birth_year"
+    "res_street_address",
+    "race_code",
+    "ethnic_code",
+    "party_cd",
+    "gender_code",
+    "birth_year",
+    "res_city_desc"
 ]
 
 # read the csv
@@ -21,8 +25,8 @@ filtered_df = df[df['res_city_desc'].str.strip().str.upper() == 'APEX']
 # remove all columns not in the keep list
 df = filtered_df[columns_to_keep]
 
-# extract the house numner and make it into a new column
-df['house_number'] = df['res_street_address'].str.extract(r'^\s*(\d+)', expand=False)
+# # extract the house numner and make it into a new column
+# df['house_number'] = df['res_street_address'].str.extract(r'^\s*(\d+)', expand=False)
 
 # save cleaned data to a new file
 df.to_csv(output_file, index=False)
